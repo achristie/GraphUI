a7559a92d10e67a427ce7c8213735b04abd1ae16
Andrew Christie <ChristiA@corp.com>
2016-04-28 15:45:48 -0400

    setup

---
+++ app/components/EntityList/entityList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class entityList extends Component {
+    render() {
+        return (
+            <ul className="list"/>
+        );
+    }
+}
+
+export default entityList;
